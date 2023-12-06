6159f706a3824df12e01e86c8cf737888da2ebea
Xphi310302 <ngxuanphi010402@gmail.com>
2023-12-06 22:48:01 +0700

    test1

--- public/script.js
+++ public/script.js
@@ -1,8 +1,35 @@
 const imageInput = document.getElementById('imageInput');
 const imageCanvas = document.getElementById('imageCanvas');
 const ctx = imageCanvas.getContext('2d');
-let isDrawing = false;
-let startX, startY, width, height;
+let img;
+let isDragging = false; // Flag to track dragging action
+let mouseX, mouseY;
+let selectedBox = null;
+let boxes = []; // Array to store box coordinates
+let resizingBox = null; // Variable to track the box being resized
+
+function draw() {
+  ctx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+  ctx.drawImage(img, 0, 0, img.width, img.height);
+
+  boxes.forEach((box, index) => {
+    ctx.strokeStyle = selectedBox === box ? 'blue' : 'red';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(box.startX, box.startY, box.width, box.height);
+
+    // Draw resize handles or additional UI elements if needed
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(box.startX + box.width, box.startY + box.height, 6, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.stroke();
+  });
+
+  if (resizingBox !== null) {
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
+    ctx.fillRect(resizingBox.startX, resizingBox.startY, resizingBox.width, resizingBox.height);
+  }
+}
 
 imageInput.addEventListener('change', function (event) {
   const file = event.target.files[0];
@@ -11,7 +38,7 @@ imageInput.addEventListener('change', function (event) {
     const reader = new FileReader();
 
     reader.onload = function (e) {
-      const img = new Image();
+      img = new Image();
 
       img.onload = function () {
         imageCanvas.width = img.width;
@@ -26,34 +53,136 @@ imageInput.addEventListener('change', function (event) {
   }
 });
 
-// Function to draw a box on the canvas
-function draw() {
-  ctx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
-  ctx.drawImage(img, 0, 0);
-
-  if (isDrawing) {
-    ctx.strokeStyle = 'red';
-    ctx.lineWidth = 2;
-    ctx.strokeRect(startX, startY, width, height);
-  }
-}
-
 imageCanvas.addEventListener('mousedown', (e) => {
-  isDrawing = true;
-  startX = e.offsetX;
-  startY = e.offsetY;
-  width = 0;
-  height = 0;
+  const rect = imageCanvas.getBoundingClientRect();
+  mouseX = e.clientX - rect.left;
+  mouseY = e.clientY - rect.top;
+
+  const { type, boxIndex } = findHandle(mouseX, mouseY);
+
+  if (type === 'resize') {
+    isDragging = false;
+    isResizing = true;
+    resizingBox = boxes[boxIndex];
+    selectedBox = resizingBox; // Select the box when clicking on the resize handle
+  } else if (type === 'drag') {
+    isDragging = true;
+    selectedBox = boxes[boxIndex];
+  } else {
+    if (selectedBox !== null && selectedBox === boxes[boxIndex]) {
+      // If the clicked box is already selected, deselect it
+      selectedBox = null;
+    } else {
+      isDragging = true;
+      const box = { startX: mouseX, startY: mouseY, width: 0, height: 0 };
+      boxes.push(box);
+      selectedBox = box;
+    }
+  }
+
+  draw();
 });
 
+
 imageCanvas.addEventListener('mousemove', (e) => {
-  if (isDrawing) {
-    width = e.offsetX - startX;
-    height = e.offsetY - startY;
+  const rect = imageCanvas.getBoundingClientRect();
+  mouseX = e.clientX - rect.left;
+  mouseY = e.clientY - rect.top;
+
+  if (isDragging) {
+    if (selectedBox !== null && !isResizing) {
+      const dx = mouseX - selectedBox.startX;
+      const dy = mouseY - selectedBox.startY;
+      selectedBox.startX = mouseX;
+      selectedBox.startY = mouseY;
+      selectedBox.width -= dx;
+      selectedBox.height -= dy;
+    } else if (isResizing && resizingBox !== null) {
+      resizingBox.width = Math.max(0, mouseX - resizingBox.startX);
+      resizingBox.height = Math.max(0, mouseY - resizingBox.startY);
+    }
     draw();
   }
 });
 
+
+
+
+
 imageCanvas.addEventListener('mouseup', () => {
-  isDrawing = false;
+  if (isDragging || isResizing) {
+    isDragging = false;
+    isResizing = false;
+    resizingBox = null;
+    draw();
+  }
 });
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Delete') {
+    if (selectedBox !== null) {
+      const index = boxes.indexOf(selectedBox);
+      if (index !== -1) {
+        boxes.splice(index, 1);
+        selectedBox = null;
+        draw();
+      }
+    }
+  }
+});
+
+imageCanvas.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+  const rect = imageCanvas.getBoundingClientRect();
+  mouseX = e.clientX - rect.left;
+  mouseY = e.clientY - rect.top;
+
+  const boxIndex = findBox(mouseX, mouseY);
+  if (boxIndex !== -1) {
+    boxes.splice(boxIndex, 1);
+    draw();
+  }
+});
+
+
+function findHandle(x, y) {
+  for (let i = 0; i < boxes.length; i++) {
+    const box = boxes[i];
+    const resizeHandleX = box.startX + box.width;
+    const resizeHandleY = box.startY + box.height;
+
+    // Check if the click is within the circle's region
+    const distX = x - resizeHandleX;
+    const distY = y - resizeHandleY;
+    const distance = Math.sqrt(distX * distX + distY * distY);
+
+    if (distance <= 6) {
+      return { type: 'resize', boxIndex: i };
+    }
+
+    if (
+      x >= box.startX &&
+      x <= box.startX + box.width &&
+      y >= box.startY &&
+      y <= box.startY + box.height
+    ) {
+      return { type: 'drag', boxIndex: i };
+    }
+  }
+  return { type: 'none', boxIndex: -1 };
+}
+
+function findBox(x, y) {
+  for (let i = 0; i < boxes.length; i++) {
+    const box = boxes[i];
+    if (
+      x >= box.startX &&
+      x <= box.startX + box.width &&
+      y >= box.startY &&
+      y <= box.startY + box.height
+    ) {
+      return i;
+    }
+  }
+  return -1;
+}
